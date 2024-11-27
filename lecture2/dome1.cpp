4db3c5770ff91fab45e8da60b15c37b9826d42ab
maque <maque8019@gmail.com>
2024-11-27 19:33:44 +0800

    This is a example of topic in Chapter 2 of CS106L

---
+++ lecture2/dome1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<utility>
+#include<cmath>
+int main(){
+	std::cout<<"Hello World"<<std::endl;
+	std::cout<<"Welcome to"<<std::endl;
+	for(char ch:"CS106L")
+	{
+		std::cout<<ch<<std::endl;
+	}
+}
+
---
+++ lecture2/dome2.cpp
@@ -0,0 +1,19 @@
+struct StandfordID{
+	string name;
+	string sunet;
+	int idNumber;
+};
+StandofordID id;
+id.name="maque";
+id.sunet="jtrb";
+id.idNumber=111111;
+struct Point{
+	double x;
+	double y;
+};
+struct Circle{
+	Point center;
+	double radius;
+};
+Circle circle{{0,0},2.0};
+
