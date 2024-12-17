d62f69534d3e7b23eb679548f38debc3882b6068
maque <maque8019@gmail.com>
2024-12-17 19:37:02 +0800

    complete the assign6

--- assign6/main.cpp
+++ assign6/main.cpp
@@ -7,7 +7,7 @@
 #include <algorithm>
 #include <type_traits>
 #include <vector>
-
+#include<optional>
 /** STUDENT_TODO: You will need to include a relevant header file here! */
 
 #include "autograder/utils.hpp"
@@ -52,10 +52,18 @@ public:
    * @param course_title The title of the course to find.
    * @return You will need to figure this out!
    */
-  FillMeIn find_course(std::string course_title)
+  std::optional<Course>  find_course(std::string course_title)
   {
     /* STUDENT_TODO: Implement this method! You will need to change the return
      * type. */
+	auto begin=courses.begin();
+	auto end=courses.end();
+	auto course=std::find_if(begin,end,[course_title](const Course& x){return x.title==course_title;});
+	if(course!=courses.end()){
+		return *course;
+	}else{
+		return std::nullopt;
+	}
     throw std::runtime_error("find_course not implemented");
   }
 
@@ -79,7 +87,7 @@ main(int argc, char* argv[])
 
     /* STUDENT_TODO: Change this condition. How can you check if the database
      * has the desired course? */
-    if (false) {
+    if (course) {
       std::cout << "Found course: " << course->title << ","
                 << course->number_of_units << "," << course->quarter << "\n";
     } else {
